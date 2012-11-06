254cebb6d33474b5c150d60ad24511c966abc059
Andrew Brandt <Andrew@Ansible-Kaze.local>
2012-11-06 11:55:02 -0500

    Add JFrame

---
+++ PathFinder.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+
+public class PathFinder
+{
+
+	public static void displayGUI()
+	{
+		
+		JFrame window = new JFrame("Path Finder");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		window.setVisible(true);
+
+	}
+
+	public static void main(String[] args)
+	{
+		
+		displayGUI();
+		
+	}
+
+}
